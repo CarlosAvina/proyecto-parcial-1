c0731ee070c0761fc5478f9f3f0a4d8e37d60ef4
Carlos Aviña <carlosavilu11@gmail.com>
2019-03-18 10:49:30 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var multer = require('multer');
+var fs = require('fs-extra');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var upload = multer({dest: 'public/files'});
+
+var app = express();
+
+// view engine setup
+app.engine('html', require('ejs').renderFile);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+//MULTER
+app.post('/profile', upload.single(), function(req, res, next) {
+  console.log(req.file);
+  console.log(req.body);
+  console.log(res);
+  console.log(next);
+});
+
+app.post('/hola', function(){
+  console.log('Hola mundo');
+});
+
+module.exports = app;
---
+++ bin/scripts/script.py
@@ -0,0 +1,5 @@
+import sys
+x=sys.argv
+x.remove(x[0])
+y = int(x[0])+1
+print(y)
