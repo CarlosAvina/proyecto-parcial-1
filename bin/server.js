f59fdee79a9ce7866a44a9be0f1c1270c38885f6
Carlos Aviña <carlosavilu11@gmail.com>
2019-03-18 19:28:50 -0600

    push para raymon

--- bin/server.js
+++ bin/server.js
@@ -1,11 +1,13 @@
 var io = require('socket.io')();
 var fs = require('fs');
 
+var ruta = '/Users/juancarlosavinaluna/Documents/ISSC/Semestre6/SistemasGeoreferenciados/proyecto-parcial-1/public/files/';
+
 io.sockets.on('connection', function(socket){
     console.log('Hola amigo ' + socket.id);
 
     socket.on('action', function(data){
-        var readFile = fs.readFileSync('/Users/juancarlosavinaluna/Documents/ISSC/Semestre6/SistemasGeoreferenciados/proyecto-parcial-1/bin/edificio_D.dat', 'utf-8');
+        var readFile = fs.readFileSync(ruta + 'areas_deportivas.dat', 'utf-8');
         console.log(readFile);
     });
 });
